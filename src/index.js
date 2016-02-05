1f81f5ff6150140ac0ec2d35a3bd9f906b4891c2
Kai Curry <kai@cape.io>
2016-02-05 16:24:40 -0600

    init commit

---
+++ src/index.js
@@ -0,0 +1,129 @@
+import immutable from 'seamless-immutable'
+
+export middleware from './middleware'
+
+export const CONNECT = 'socket/CONNECT'
+export const DISCONNECT = 'socket/DISCONNECT'
+export const CONNECTED = 'socket/CONNECTED'
+export const FEEDBACK = 'socket/FEEDBACK'
+export const JOINED = 'socket/JOINED'
+export const SUBSCRIBE = 'socket/SUBSCRIBE'
+export const GUEST_JOIN = 'socket/GUEST_JOIN'
+export const GUEST_LEAVE = 'socket/GUEST_LEAVE'
+export const API_FAILURE = 'socket/API_FAILURE'
+export const API_REQUEST = 'socket/API_REQUEST'
+export const API_SUCCESS = 'socket/API_SUCCESS'
+
+const defaultState = immutable({
+  connected: false,
+  endTime: null,
+  presenter: null,
+  sessionId: null,
+  siteId: null,
+  socketId: null,
+  startTime: null,
+  subscriber: {},
+})
+function wrapState(state, action) {
+  if (!action.type) return state
+  return state.asMutable ? state : immutable(state)
+}
+export default function reducer(_state = defaultState, action) {
+  const { type, payload } = action
+  const state = wrapState(_state, action)
+  switch (type) {
+    case '@@INIT':
+      return state.asMutable ? state : immutable(state)
+    case CONNECT:
+      return state.set('connected', true)
+    case DISCONNECT:
+      return state.merge({ connected: false, socketId: null, endTime: payload })
+    case CONNECTED:
+      // Is it stupid to also set connected to true? Used on backend.
+      return state.set('connected', true).merge(payload)
+    case JOINED:
+      return state.set('sessionId', payload)
+    case SUBSCRIBE:
+      return state.set('presenter', payload)
+    case GUEST_JOIN:
+      return state.setIn([ 'subscriber', payload ], { id: payload })
+    case GUEST_LEAVE:
+      return state.set('subscriber', state.subscriber.without(payload))
+    default:
+      return state
+  }
+}
+
+export function idReducer(state = null, action) {
+  if (action.type === CONNECTED && action.payload.socketId) {
+    return action.payload.socketId
+  }
+  return state
+}
+
+export function connect(payload) {
+  return {
+    type: CONNECT,
+    payload,
+  }
+}
+export function connected(socketId, siteId) {
+  const now = new Date()
+  return {
+    type: CONNECTED,
+    payload: {
+      siteId,
+      socketId,
+      startTime: now.toString(),
+    },
+  }
+}
+export function disconnect() {
+  const now = new Date()
+  return {
+    type: DISCONNECT,
+    payload: now.toString(),
+    sendSocket: false,
+  }
+}
+export function feedback(message) {
+  return {
+    type: FEEDBACK,
+    payload: message,
+  }
+}
+export function guestJoined(id) {
+  return {
+    type: GUEST_JOIN,
+    payload: id,
+  }
+}
+export function joined(sessionId) {
+  return {
+    type: JOINED,
+    payload: sessionId,
+  }
+}
+export function subscribe(presenter) {
+  return {
+    type: SUBSCRIBE,
+    payload: presenter,
+  }
+}
+export function apiFailure() {
+  return {
+    type: API_FAILURE,
+  }
+}
+export function apiRequest(payload) {
+  return {
+    type: API_REQUEST,
+    payload,
+  }
+}
+export function apiSuccess(entities) {
+  return {
+    type: API_SUCCESS,
+    response: { entities },
+  }
+}
---
+++ src/middleware.js
@@ -0,0 +1,74 @@
+import { connect, disconnect, JOINED } from './index'
+
+function getSessionId() {
+  return sessionStorage.sessionId || null
+}
+// Insert sessionId.
+function setSessionId(id) {
+  // console.log('setSessionId', id)
+  sessionStorage.sessionId = id
+}
+
+// Should save presenter to sessionStorage too?
+
+// Socket.io socket = io(opts.location)
+export default function createSocketMiddleware(socket, options = {}) {
+  // Defaults.
+  const opts = {
+    eventName: 'action',
+    ...options,
+  }
+
+  return store => {
+    // Wire socket.io to dispatch actions sent by the server.
+    socket.on(opts.eventName, (action) => {
+      const act = action
+      // If an action comes from the server do not send it back to the server.
+      act.sendSocket = false
+      store.dispatch(act)
+      switch (action.type) {
+        // Joined. Server returns valid sessionId.
+        case JOINED:
+          setSessionId(action.payload)
+          break
+        default:
+      }
+    })
+    // When the connection is established. Before any events.
+    socket.on('connect', () => {
+      // Tell local state we are connected.
+      store.dispatch(connect({
+        // Send url query information.
+        // page: store.getState().filter.page,
+        // Tell server result of our local sessionId.
+        sessionId: getSessionId(),
+      }))
+    })
+    // Tell redux we are no longer connected.
+    socket.on('disconnect', () => {
+      store.dispatch(disconnect())
+    })
+
+    return next => action => {
+      const { emit, sendSocket, type, payload } = action
+      // We do not care about any action without type prop set.
+      if (!type) return next(action)
+      // Pass along any action with sendSocket set to false.
+      if (sendSocket === false || type.endsWith('_SUCCESS')) {
+        return next(action)
+      }
+      // Allow actions to send an emit to server.
+      if (typeof sendSocket === 'string') {
+        socket.emit(sendSocket, emit || payload)
+      }
+      // Prevent location actions from trigger.
+      if (store.getState().socket.presenter) {
+        console.log('Subscribe mode prevented local action', type)
+      } else {
+        // Send most every action to the server. Whoa.
+        socket.emit(opts.eventName, action)
+        return next(action)
+      }
+    }
+  }
+}
