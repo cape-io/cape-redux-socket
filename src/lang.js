67a24f2236ad109e73911421be9c4004522d474c
Kai Curry <kai@cape.io>
2016-09-14 20:03:46 -0500

    move comments

--- src/lang.js
+++ src/lang.js
@@ -12,14 +12,13 @@ export const isFalse = identical(false)
 export function getSendSocket({ sendSocket, meta }) {
   return sendSocket || result(meta, 'sendSocket')
 }
-// Prevent socket emit when sendSocket is set to false.
+// Require sendSocket to not be a strict false.
 export const allowEmit = flow(getSendSocket, negate(isFalse))
 export const sendIsString = flow(getSendSocket, isString)
+// We do not care about any action without type prop set.
 export const validAction = overEvery([ property('type'), allowEmit ])
 export const noPresenter = flow(result('getState'), negate(getPresenter))
 export function doEmitAction(action) {
-  // We do not care about any action without type prop set.
-  // Also check for false sendSocket or presenter.
   return overEvery([ validAction, noPresenter(action) ])
 }
 export const doEmitEvent = overEvery([ validAction, sendIsString ])
