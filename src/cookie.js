87957d05b5314d219eabb642d209937a4be6506e
Kai Curry <kai@cape.io>
2016-09-22 22:09:46 -0500

    cookie middleware improvements

--- src/cookie.js
+++ src/cookie.js
@@ -1,30 +1,33 @@
-import isEmpty from 'lodash/isEmpty'
-import merge from 'lodash/merge'
+import { isEmpty, set, unset } from 'lodash'
 import Cookies from 'js-cookie'
 
-// Look in action.meta for cookie.
-function setSid(meta) {
-  const { name, value, options } = meta.cookie
+import { hasCookie } from './lang'
+
+// Handle action.meta.cookie.
+function setCookie(action) {
+  const { name, value, options } = action.meta.cookie
   if (value) {
     Cookies.set(name, value, options)
   } else {
     Cookies.remove(name, options)
   }
   // Remove the cookie from the action. Do not send it along to other middleware or reducers.
-  delete meta.cookie // eslint-disable-line no-param-reassign
+  // Why should it be removed? Makes things harder to debug.
+  unset(action, 'meta.cookie')
+  return action
 }
 
 export function addCookieMeta(action) {
   const cookieJar = Cookies.get()
   if (isEmpty(cookieJar)) return action
   // Add cookie information to action sent to server.
-  return merge({}, action, { meta: { cookie: cookieJar } })
+  return set(action, 'meta.cookie', cookieJar)
 }
 
 // Look into every action for meta.cookie.
 export function cookieMiddleware() {
   return next => (action) => {
-    if (action.meta && action.meta.cookie) setSid(action.meta)
-    next(action)
+    if (hasCookie(action)) return next(setCookie(action))
+    return next(action)
   }
 }
